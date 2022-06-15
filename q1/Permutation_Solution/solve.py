82c5ab2f42d00c7de56731a16551aa35a770ec04
Danniel Henrique Correa Vieira <danniel@helpetapp.com>
2022-06-15 07:51:01 -0300

    Added Q1 permutation

--- q1/Permutation_Solution/solve.py
+++ q1/Permutation_Solution/solve.py
@@ -1,6 +1,8 @@
+import time
 class Graph:
     def __init__(self):
         self.graph = {}
+        self.numComparison = 0
 
     def add_edge(self, s, d):
         if(s not in self.graph):
@@ -15,7 +17,12 @@ class Graph:
             print("%s: -> %s" % (i, self.graph[i]))
 
     def permutation_cycles(self):
-        return self.permutation_cyclesR(self.graph, [])
+        self.numComparison = 0
+        start_time = time.time_ns()
+        qtd = self.permutation_cyclesR(self.graph, [])
+        end_time = time.time_ns()
+        print("Time of Execution: {}".format((end_time - start_time) / 10**6))
+        return qtd
 
     def permutation_cyclesR(self, li, perm):
         cont = 0
@@ -24,21 +31,24 @@ class Graph:
             if(v in perm):
                 if(v == perm[0] and len(perm) > 2):
                     cont += 1
-                    
+                self.numComparison += 3
             else:
                 perm.append(v)
                 cont += self.permutation_cyclesR(self.graph[v], perm)
                 perm.pop()
-                
+            self.numComparison += 1
         return cont
 
 if __name__ == "__main__":
     graph = Graph()
-    graph.add_edge('A', 'B')
-    graph.add_edge('A', 'C')
-    graph.add_edge('A', 'D')
-    graph.add_edge('B', 'C')
-    graph.add_edge('B', 'D')
-    graph.add_edge('C', 'D')
+    print("Counting cycles in undirected graph.\n")
+    while True:
+        e1, e2 = input("Input edge (edge A, edge B) (, to escape): ").split(",")
+        if len(e1) == 0:
+            break
+        e1 = e1.strip()
+        e2 = e2.strip()
+        graph.add_edge(e1, e2)
 
-    print(graph.permutation_cycles())
+    print("Number of cycles: {}".format(graph.permutation_cycles()))
+    print("Number of comparisons: {}".format(graph.numComparison))
