3d63fe4d754078648c9c35525264f415f7c254df
unknown <dannielHCVieira>
2022-06-15 22:37:45 -0300

    Added plot

--- q1/solve.py
+++ q1/solve.py
@@ -16,12 +16,23 @@ class Graph:
         for i in self.graph:
             print("%s: -> %s" % (i, self.graph[i]))
 
+    def fill_graph(self, n):
+        for i in range(0, n):
+            for j in range(i+1, n):
+                self.add_edge("e{}".format(i),"e{}".format(j))
+
     def permutation_cycles(self):
         start_time = time.time_ns()
         qtd = self.permutation_cyclesR(self.graph, [])
         end_time = time.time_ns()
         print("Time of Execution: {}".format((end_time - start_time) / 10**6))
         return qtd
+
+    def permutation_cycles_time(self):
+        start_time = time.time_ns()
+        qtd = self.permutation_cyclesR(self.graph, [])
+        end_time = time.time_ns()
+        return (end_time - start_time) / 10**6
 
     def permutation_cyclesR(self, li, perm):
         cont = 0
@@ -64,6 +75,19 @@ class Graph:
         print("Time of Execution: {}".format((end_time - start_time) / 10**6))
         return c
 
+    def DFS_cycles_time(self):
+        c = 0
+        start_time = time.time_ns()
+        visited = {}
+        for k in self.graph.keys():
+            visited[k] = False
+ 
+        for k in self.graph.keys():
+            if visited[k] == False:
+                c += self.DFS_cyclesR(k, visited, -1)
+
+        end_time = time.time_ns()
+        return (end_time - start_time) / 10**6
 if __name__ == "__main__":
     print("Counting cycles in undirected graph.\n")
     op0 = int(input("1 - Solution with Permutation\n2 - Solution with DFS\n0 - Exit\n>>> "))
@@ -74,9 +98,7 @@ if __name__ == "__main__":
                 graph = Graph()
                 if op == 1:
                     n = int(input("How many vertex? "))
-                    for i in range(0, n):
-                        for j in range(i+1, n):
-                            graph.add_edge("e{}".format(i),"e{}".format(j))
+                    graph.fill_graph()
                     print("Number of cycles: {}".format(graph.permutation_cycles()))
                 elif op == 2:
                     while True:
@@ -97,9 +119,7 @@ if __name__ == "__main__":
                 graph = Graph()
                 if op == 1:
                     n = int(input("How many vertex? "))
-                    for i in range(0, n):
-                        for j in range(i+1, n):
-                            graph.add_edge("e{}".format(i),"e{}".format(j))
+                    graph.fill_graph()
                     print("Number of cycles: {}".format(graph.DFS_cycles()))
                 elif op == 2:
                     while True:
